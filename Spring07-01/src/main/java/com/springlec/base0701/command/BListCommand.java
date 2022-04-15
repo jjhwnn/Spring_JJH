c84ab9a963227a98fbee34cd88b17aeee74524dd
jjhwnn <jeahwan174@gmail.com>
2022-04-15 18:38:47 +0900

    update BListCommand

--- Spring07-01/src/main/java/com/springlec/base0701/command/BListCommand.java
+++ Spring07-01/src/main/java/com/springlec/base0701/command/BListCommand.java
@@ -11,11 +11,11 @@ public class BListCommand implements BCommand {
 
 	@Override
 	public void execute(Model model) {
-
+		
 		//----
 		BDao dao = new BDao();
 		ArrayList<BDto> dtos = dao.list();
-
+		
 		model.addAttribute("list", dtos);
 		
 	}
